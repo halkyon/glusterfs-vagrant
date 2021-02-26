0ae80e02742cc612db555bf6fbe195f2543fe2f3
Sean Harvey <halkyon@gmail.com>
2021-02-26 04:05:55 +0000

    minor change to apt -q switch

--- provision-common.sh
+++ provision-common.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 apt-get -q update
-DEBIAN_FRONTEND=noninteractive apt-get -qq -y -o Dpkg::Options::="--force-confnew" dist-upgrade
+DEBIAN_FRONTEND=noninteractive apt-get -q -y -o Dpkg::Options::="--force-confnew" dist-upgrade
 apt-get -q -y --purge autoremove
 apt-get autoclean
 
